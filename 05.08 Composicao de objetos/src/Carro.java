aa0c210d7064ed2bedd59415fd4dfe502146638c
Artur Aires <166089713+ArturAires8@users.noreply.github.com>
2025-05-15 13:08:32 -0300

    Add files via upload

---
+++ 05.08 Composicao de objetos/src/Carro.java
@@ -0,0 +1,11 @@
+public class Carro {
+
+
+    String fabricante;
+    String modelo;
+    String cor;
+    int anoFabricaccao;
+    Pessoa proprietario;
+
+
+}
---
+++ 05.08 Composicao de objetos/src/Principal.java
@@ -0,0 +1,35 @@
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+
+        Carro meuCarro = new Carro();
+
+        meuCarro.anoFabricaccao = 2021;
+        meuCarro.cor = "Preto";
+        meuCarro.fabricante = "Honda";
+        meuCarro.modelo = "HR-V";
+
+        Carro seuCarro = new Carro();
+
+        seuCarro.anoFabricaccao = 2022;
+        seuCarro.cor = "Branco";
+        seuCarro.fabricante = "BMW";
+        seuCarro.modelo = "X6";
+
+
+        System.out.println("Meu Carro");
+        System.out.println("-------------");
+        System.out.printf("Modelo: %s%n", meuCarro.modelo);
+        System.out.printf("Ano: %d%n", meuCarro.anoFabricaccao);
+
+        System.out.println();
+
+        System.out.println("Seu Carro");
+        System.out.println("-------------");
+        System.out.printf("Modelo: %s%n", seuCarro.modelo);
+        System.out.printf("Ano: %d%n", seuCarro.anoFabricaccao);
+
+    }
+}
