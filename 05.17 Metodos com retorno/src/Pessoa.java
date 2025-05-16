be38adaca4da0e3b306ec66a3ebf16961dc1d9b0
Artur Aires <166089713+ArturAires8@users.noreply.github.com>
2025-05-16 12:23:27 -0300

    Add files via upload

---
+++ 05.17 Metodos com retorno/src/Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+
+
+    String nome;
+    String cpf;
+    int anoNascimento;
+
+
+
+
+}
---
+++ 05.17 Metodos com retorno/src/Principal.java
@@ -0,0 +1,67 @@
+
+public class Principal {
+    public static void main(String[] args) {
+
+
+        Pessoa eu = new Pessoa();
+        eu.nome = "Artur Aires";
+        eu.cpf = "01709698195";
+        eu.anoNascimento = 1994;
+
+        Pessoa voce = new Pessoa();
+        voce.nome = "Bartolomeu";
+        voce.cpf = "01709698196";
+        voce.anoNascimento = 2013;
+
+
+        Carro meuCarro = new Carro();
+        meuCarro.anoFabricacao = 2021;
+        meuCarro.cor = "Preto";
+        meuCarro.fabricante = "Honda";
+        meuCarro.modelo = "HR-V";
+        meuCarro.precoCompra = 120000;
+        meuCarro.proprietario = eu;
+
+
+//        meuCarro.proprietario = new Pessoa();
+//        meuCarro.proprietario.nome = "Artur";
+//        meuCarro.proprietario.cpf = "01709698195";
+//        meuCarro.proprietario.anoNascimento = 1994;
+
+        Carro seuCarro = new Carro();
+
+        seuCarro.anoFabricacao = 2022;
+        seuCarro.cor = "Branco";
+        seuCarro.fabricante = "BMW";
+        seuCarro.modelo = "X6";
+        seuCarro.precoCompra = 980000;
+        seuCarro.proprietario = voce;
+
+        double valorRevenda = meuCarro.calcularValorRevenda();
+
+
+        System.out.printf("Tempo de uso (anos) : %d%n", 2022 - meuCarro.anoFabricacao);
+
+        System.out.printf("Valor de revenda: %6.2f%n", valorRevenda);
+
+
+//        seuCarro.calcularValorRevenda();
+
+
+//        System.out.println("Meu Carro");
+//        System.out.println("-------------");
+//        System.out.printf("Modelo: %s%n", meuCarro.modelo);
+//        System.out.printf("Ano: %d%n", meuCarro.anoFabricacao);
+//        System.out.printf("Proprietario: %s%n", meuCarro.proprietario.nome);
+//
+//
+//        System.out.println();
+//
+//        System.out.println("Seu Carro");
+//        System.out.println("-------------");
+//        System.out.printf("Modelo: %s%n", seuCarro.modelo);
+//        System.out.printf("Ano: %d%n", seuCarro.anoFabricacao);
+//        System.out.printf("Proprietario: %s%n", seuCarro.proprietario.nome);
+
+    }
+}
