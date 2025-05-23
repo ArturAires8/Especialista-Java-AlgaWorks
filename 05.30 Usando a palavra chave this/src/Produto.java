f92514eb99135b1e1b31307a75efd2708a563569
Artur Aires <166089713+ArturAires8@users.noreply.github.com>
2025-05-23 19:59:14 -0300

    Add files via upload

---
+++ 05.30 Usando a palavra chave this/src/Produto.java
@@ -0,0 +1,13 @@
+public class Produto {
+
+    double precoCusto;
+    double precoVenda;
+
+    void alterarPrecoCusto(double precoCusto) {
+        this.precoCusto = precoCusto;
+
+
+    }
+
+
+}
---
+++ 05.30 Usando a palavra chave this/src/ServicoDePrecificacao.java
@@ -0,0 +1,17 @@
+public class ServicoDePrecificacao {
+
+
+    void definirPrecoVenda(Produto produto, double percentualMargemLucro) {
+
+        // poderia ter cálculos muito mais complexos aqui
+
+        produto.precoVenda = produto.precoCusto * ((percentualMargemLucro / 100) + 1);
+
+    }
+
+
+}
+
+
+
+
