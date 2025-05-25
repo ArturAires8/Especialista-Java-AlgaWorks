be742855a3fa754dfe329002ed375045997c31c5
Artur Aires <166089713+ArturAires8@users.noreply.github.com>
2025-05-25 14:04:07 -0300

    Add files via upload

---
+++ 05.32 Metodo de instancia alterando variavel estatica/src/ServicoDePrecificacao.java
@@ -0,0 +1,16 @@
+public class ServicoDePrecificacao {
+
+    void definirPrecoVenda(Produto produto, double percentualMargemLucro) {
+        // poderia ter cálculos muito mais complexos aqui
+
+        double precoVendaCaulculado = produto.precoCusto * ((percentualMargemLucro / 100) + 1);
+        precoVendaCaulculado += Produto.custoEmbalagem;
+
+        produto.precoVenda = precoVendaCaulculado;
+
+    }
+
+}
+
+
+
---
+++ 05.33 Metodos de classe estaticos/src/ServicoDePrecificacao.java
@@ -0,0 +1,16 @@
+public class ServicoDePrecificacao {
+
+    void definirPrecoVenda(Produto produto, double percentualMargemLucro) {
+        // poderia ter cálculos muito mais complexos aqui
+
+        double precoVendaCaulculado = Matematica.calcularAcrescimo(produto.precoCusto, percentualMargemLucro);
+
+        precoVendaCaulculado += Produto.custoEmbalagem;
+
+        produto.precoVenda = precoVendaCaulculado;
+
+    }
+
+}
+
+
