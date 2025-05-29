086661a8420e76b425bb8d1e609d320b011875bd
Artur Aires <166089713+ArturAires8@users.noreply.github.com>
2025-05-28 22:07:21 -0300

    Add files via upload

---
+++ 11.01 O problema de falta de encapsulamento/src/com/algaworks/cartaobeneficio/servico/ServicoDePagamentoOnline.java
@@ -0,0 +1,26 @@
+package com.algaworks.cartaobeneficio.servico;
+
+import com.algaworks.cartaobeneficio.Cartao;
+import com.algaworks.cartaobeneficio.Estabelecimento;
+import com.algaworks.cartaobeneficio.Recibo;
+
+public class ServicoDePagamentoOnline {
+
+    public Recibo efetuarPagamento(Estabelecimento estabelecimento,
+                                   Cartao cartao, double valor) {
+
+        if (cartao.saldo < valor) {
+            throw new RuntimeException("Saldo insuficiente para pagamento");
+        }
+
+
+        cartao.saldo -= valor;
+
+        // TODO realiza outras lógicas para efetuar o pagamento ao estabelecimento
+
+        return new Recibo(cartao.titular, "Pagamento", valor);
+
+    }
+
+
+}
