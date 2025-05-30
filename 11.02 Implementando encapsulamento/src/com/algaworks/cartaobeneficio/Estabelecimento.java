0d9e25fee649665f9c9659ef56ce228e5df734bc
Artur Aires <166089713+ArturAires8@users.noreply.github.com>
2025-05-29 21:00:01 -0300

    Add files via upload

---
+++ 11.02 Implementando encapsulamento/src/com/algaworks/cartaobeneficio/Estabelecimento.java
@@ -0,0 +1,18 @@
+package com.algaworks.cartaobeneficio;
+
+public class Estabelecimento {
+
+
+    public String nome;
+    public double saldo;
+
+    public Estabelecimento(String nome) {
+        this.nome = nome;
+    }
+
+}
+
+
+
+
+
---
+++ 11.02 Implementando encapsulamento/src/com/algaworks/cartaobeneficio/servico/ServicoDePagamentoOnline.java
@@ -0,0 +1,24 @@
+package com.algaworks.cartaobeneficio.servico;
+
+import com.algaworks.cartaobeneficio.Cartao;
+import com.algaworks.cartaobeneficio.Estabelecimento;
+import com.algaworks.cartaobeneficio.Recibo;
+
+public class ServicoDePagamentoOnline {
+
+    public Recibo efetuarPagamento(Estabelecimento estabelecimento,
+                                   Cartao cartao, double valor) {
+
+        cartao.debitar(valor);
+
+
+        // cartao.saldo -= valor;
+
+        // TODO realiza outras lógicas para efetuar o pagamento ao estabelecimento
+
+        return new Recibo(cartao.obterTitular(), "Pagamento", valor);
+
+    }
+
+
+}
