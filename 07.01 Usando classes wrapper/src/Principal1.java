53f50eb6bb3e6460d2956ec68840476d9e2f4ca5
Artur Aires <166089713+ArturAires8@users.noreply.github.com>
2025-06-15 17:05:52 -0300

    Add files via upload

---
+++ 07.01 Usando classes wrapper/src/Principal1.java
@@ -0,0 +1,21 @@
+public class Principal1 {
+
+    public static void main(String[] args) {
+
+        // tipos primitivos
+
+        Integer diasParaEntrega;
+        Long codigoEntrega;
+        Float valorFrete;
+        Double valorTotal;
+        Character tipoCliente;
+        Boolean compraPaga;
+
+        // String é uma classe
+        String nomeCliente;
+
+
+    }
+
+
+}
---
+++ 07.02 Metodos de conversao/src/Principal.java
@@ -0,0 +1,19 @@
+
+public class Principal {
+    public static void main(String[] args) {
+
+        int idade = 20;
+        short idadeShort = (short) idade;
+
+
+        Integer diasEntrega = Integer.valueOf(30);
+        Short diasEntregaShort = Short.valueOf(diasEntrega.shortValue());
+        Long diasEntregasLong = Long.valueOf(diasEntrega.longValue());
+
+
+        Double valorTotal = Double.valueOf(1500.2);
+        int valorTotalInt = valorTotal.intValue();
+        System.out.println(valorTotalInt);
+
+    }
+}
