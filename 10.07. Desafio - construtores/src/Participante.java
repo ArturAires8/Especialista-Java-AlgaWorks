6b1923e4d34f7c717822211c71242970dd0244a4
Artur Aires <166089713+ArturAires8@users.noreply.github.com>
2026-02-19 14:57:37 -0300

    Add files via upload

---
+++ 10.07. Desafio - construtores/src/Participante.java
@@ -0,0 +1,24 @@
+import java.util.Objects;
+
+public class Participante {
+
+
+    String nome;
+    int saldoDePontos;
+
+    Participante(String nome) {
+        this(nome, 0);
+    }
+
+    Participante(String nome, int saldoDePontosInicial) {
+        Objects.requireNonNull(nome, "Nome é obrigatório");
+
+        if (saldoDePontosInicial < 0) {
+            throw new IllegalArgumentException("Saldo de pontos inicial não pode ser negativo");
+        }
+
+        this.nome = nome;
+        this.saldoDePontos = saldoDePontosInicial;
+    }
+
+}
